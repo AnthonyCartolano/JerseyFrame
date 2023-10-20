91730580894f22c7a53ac1c23282bfb03da6378c
AnthonyCartolano <148574599+AnthonyCartolano@users.noreply.github.com>
2023-10-20 12:53:17 -0400

    Add files via upload

---
+++ Purple.ino
@@ -0,0 +1,19 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+#define PIN 6
+#define NUMPIXELS 20
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  pixels.begin();
+}
+
+void loop() {
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(128, 0, 128));
+    pixels.show();
+
+  }
+}
